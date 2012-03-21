801cd7a8ac5e1f731308183fa56b052d6a5c9df7
mscstephen <106370080@umail.ucc.ie>
2012-03-21 13:02:06 +0000

    fixed a datacentre bug in assignment

--- src/pure_thermal/DataCenter.java
+++ src/pure_thermal/DataCenter.java
@@ -1,60 +1,68 @@
-package pure_thermal;
+package thermalproject.pure_thermal;
 
 /**
  *
  * @author fsmm2
  */
 import java.util.*;
-import pure_thermal.ThermalCPU;
-import pure_thermal.Air;
 
 public class DataCenter {
 
     int count = 0;
-    private int[] coolingPlan = {0, 0, 0, 0};
-    //cooling plan from optimization. Not sure of exact format
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+    int[] coolingPlan = {0, 0, 0, 0};
     double ambientRoomTemp = 299.15;
     double aggregateTemp;
     int[] futureCooling;
-    //int array for intensityPlan is most definately PLACEHOLDER
-    double[] intensityPlan;
+    int[] intensityPlan;
     String[] racks = {"a", "b", "c", "d"};
     //enum char[] better suited for racks. don't know how.
-    private ArrayList<ThermalCPU> list;
+    ArrayList<ThermalCPU> list;
 
-    //double[] s;
+    public void setList(ArrayList<ThermalCPU> list) {
+        System.out.println(list.get(0).currentTemp);
+        for(int i=0; i<40; i++)
+        {
+            this.list.get(i).CopyCPU(list.get(i));
+            System.out.println(i);
+        }
+        System.out.println("///////////////");
+        System.out.println(this.list.size());
+        System.out.println("///////////////");
+    }
+
+    public ArrayList<ThermalCPU> getList() {
+        return list;
+    }
+    double[] s;
     Air air = new Air();
 
     public DataCenter() {
-    list = new ArrayList<ThermalCPU>();
+        list=new ArrayList<ThermalCPU>();
         int j = 0;
         while (j < 4) {
             int i = 1;
             String current = racks[j];
-            while (i < 11) {
-                //TESTING purposes: one ThermalCPU with excessive heat PLACEHOLDER: testing methods
-                if (i == 3 && j == 1) {
-                    ThermalCPU name = new ThermalCPU(i, current, 327.15, 0, true);
-                    i++;
-                    list.add(name);
-                } else {
+            while (i < 11) {              
                     ThermalCPU name = new ThermalCPU(i, current);
                     i++;
-                    list.add(name);
-                }
+                    list.add(name);              
             }
             j++;
         }
     }
 
-    public void showTempArray() //need to add useful getTempArray() PLACEHOLDER: stub
+    public void showTempArray()
     {
-        for (ThermalCPU a : getList()) {
+        for (ThermalCPU a : list) {
             System.out.println(a.getName() + ": " + (a.getTemp() - 273.15));
         }
     }
 
-    public double[][] getTempArray() {
+     public double[][] getTempArray() {
         double[][] output=new double[4][10];
         ThermalCPU c;
 
@@ -62,32 +70,42 @@ public class DataCenter {
         {
             for(int i=0; i<10; i++)
             {
-                c=getList().get(i+1);
-            output[j][i]= c.getTemp() - 273.15;
+                c=getList().get(j*10+i);
+                output[j][i]= c.getTemp() - 273.15;
             }
         }
-        
+
         return output;
     }
 
-    public void showIntensityPlan() //need to add useful getIntensityArray() PLACEHOLDER: stub
+
+
+
+    
+
+    public void showIntensityPlan()
     {
-        for (ThermalCPU a : getList()) {
+        for (ThermalCPU a : list) {
             System.out.println(a.getName() + ": " + a.getIntensity());
         }
     }
 
-    public void setIntensityPlan(double[] plan) {
+    public int[] getIntensityPlan()
+    {
+        return intensityPlan;
+    }
+
+    public void setIntensityPlan(int[] plan) {
         intensityPlan = plan;
     }
 
     public ArrayList<String> getHotspots() //should also timestamp array, as may runs will be taken
     {
         ArrayList<String> stringlist = new ArrayList<String>();
-        for (ThermalCPU a : getList()) {
+        for (ThermalCPU a : list) {
             double temp = a.getTemp();
-            //temp 343.15 is a PLACEHOLDER: group decision
-            if (temp > 343.15) {
+            //temp 343.15: group decision
+            if (temp > 323.15) {
                 String hot = "!" + a.getName() + " : " + (a.getTemp() - 273.15) + "!";
                 stringlist.add(hot);
             }
@@ -99,7 +117,7 @@ public class DataCenter {
 
     public void showHotspots() {
         ArrayList<String> secondstring = new ArrayList<String>();
-        for (ThermalCPU a : getList()) {
+        for (ThermalCPU a : list) {
             double temp = a.getTemp();
             if (temp > 343.15) {
                 String hot = "!" + a.getName() + " : " + (a.getTemp() - 273.15) + "!";
@@ -114,7 +132,7 @@ public class DataCenter {
     }
 
     public float getRoomTemp() {
-        for (int p = 0; p + 1 < getList().size(); p++) {
+        for (int p = 0; p + 1 < list.size(); p++) {
             Air airAtCpu = new Air(p);
             double temp = airAtCpu.getTemp();
             aggregateTemp = aggregateTemp + temp;
@@ -126,16 +144,14 @@ public class DataCenter {
         return roomTemp;
     }
 
-     public void updateArray(int[] cooling) {
+      public void updateArray(int[] cooling) {
         ThermalCPU current = new ThermalCPU();
         ThermalCPU next = new ThermalCPU();
 
-        for (int p = 0; p + 1 < list.size(); p++)
+        for (int p = 0; p +1< list.size(); p++)
         {
-            //Air airAroundNext = airArray[p];
             current = list.get(p);
             current.setIntensity(intensityPlan[p]);
-           // current.setIntensity(25);
             int index;
             index = p/10;
                 if (current.getPos() != (index+1)*10) {
@@ -156,21 +172,19 @@ public class DataCenter {
     }
 
     public void showArrayLength() {
-        System.out.println(getList().size());
+        System.out.println(list.size());
     }
 
     public void runArray(int i) {
-        //i=100;
-        //DataCenter d = new DataCenter();
-        for (int j = 1; j <= i; j++) {
-               updateArray(getCoolingPlan());
+        for (int j = 0; j < i; j++) {
+                this.updateArray(coolingPlan);
         }
     }
 
     public void predictArray(int i) {
-        DataCenter d= new DataCenter();
+        DataCenter d=this;
         for (int j = 0; j < i; j++) {
-            d.updateArray(getCoolingPlan());
+            d.updateArray(coolingPlan);
         }
     }
 
@@ -181,17 +195,21 @@ public class DataCenter {
             System.out.println(a);
         }
     }
-//testing PLACEHOLDER. updateCooling should update futureCooling
 
-//    public void setCoolingPlan(int[] a) {
-//        setCoolingPlan(a);
-//    }
+    public int[] getCoolingPlan()
+    {
+        return coolingPlan;
+    }
+
+    public void setCoolingPlan(int[] a) {
+        coolingPlan = a;
+    }
 
     public void showCooling() {
-        System.out.println(this.getCoolingPlan()[0]);
-        System.out.println(this.getCoolingPlan()[1]);
-        System.out.println(this.getCoolingPlan()[2]);
-        System.out.println(this.getCoolingPlan()[3]);
+        System.out.println(this.coolingPlan[0]);
+        System.out.println(this.coolingPlan[1]);
+        System.out.println(this.coolingPlan[2]);
+        System.out.println(this.coolingPlan[3]);
     }
 
 //count defines the time in the datacenter. at 30, i.e. 5 minute mark, new cooling plan is implemented and count is reset.
@@ -201,7 +219,7 @@ public class DataCenter {
 
     public double getAirTemp() {
 
-     Double airTemp= air.getTemp();
+     Double airTemp= air.getTemp()-273.15;
         return airTemp;
     }
 
@@ -212,7 +230,7 @@ public class DataCenter {
 
     public double[] flattenArray(double[][] input)
     {
-        double[] output= new double[40];
+        double[] output=null;
         int k=0;
         for(int i=0; i<input.length; i++)
         {
@@ -224,24 +242,11 @@ public class DataCenter {
         }
         return output;
     }
- public int[] flattenArray(int[][] input)
-    {
-        int[] output=null;
-        int k=0;
-        for(int i=0; i<input.length; i++)
-        {
-            for(int j=0; j<input[i].length; j++)
-            {
-                output[k]=input[i][j];
-                k++;
-            }
-        }
-        return output;
-    }
+
     public ThermalCPU getThermalCPU(String l, int pos)
     {
         ThermalCPU placeholder=new ThermalCPU();
-        for(ThermalCPU a:getList())
+        for(ThermalCPU a:list)
         {
             if(a.getPos()==pos&&a.getRack().compareTo(l)==0)
                 placeholder=a;
@@ -251,31 +256,11 @@ public class DataCenter {
 
     }
 
-    /**
-     * @return the list
-     */
-    public ArrayList<ThermalCPU> getList() {
-        return list;
-    }
-
-    /**
-     * @param list the list to set
-     */
-    public void setList(ArrayList<ThermalCPU> list) {
-        this.list = list;
-    }
-
-    /**
-     * @return the coolingPlan
-     */
-    public int[] getCoolingPlan() {
-        return coolingPlan;
-    }
-
-    /**
-     * @param coolingPlan the coolingPlan to set
-     */
-    public void setCoolingPlan(int[] coolingPlan) {
-        this.coolingPlan = coolingPlan;
+    public void getAllAttributes(DataCenter d)
+    {
+        this.setCoolingPlan(d.getCoolingPlan());
+        this.setIntensityPlan(d.getIntensityPlan());
+        this.setList(d.getList());
+        this.setCount(d.getCount());
     }
     }
